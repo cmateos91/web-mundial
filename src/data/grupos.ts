3bc9f5c80c39ceda46a15fa646a470ef8b9fa609
NeumorStudio <neumorstudio2@gmail.com>
2026-03-25 00:51:07 +0100

    feat: Mejorar estilo visual

---
+++ src/data/grupos.ts
@@ -0,0 +1,50 @@
+export const grupos = [
+  {
+    nombre: "Grupo A",
+    equipos: ["México", "Corea del Sur", "Sudáfrica", "Por definir (UEFA D)"]
+  },
+  {
+    nombre: "Grupo B",
+    equipos: ["Canadá", "Suiza", "Qatar", "Por definir (UEFA A)"]
+  },
+  {
+    nombre: "Grupo C",
+    equipos: ["Brasil", "Marruecos", "Escocia", "Haití"]
+  },
+  {
+    nombre: "Grupo D",
+    equipos: ["Estados Unidos", "Paraguay", "Australia", "Por definir (UEFA C)"]
+  },
+  {
+    nombre: "Grupo E",
+    equipos: ["Alemania", "Ecuador", "Costa de Marfil", "Curazao"]
+  },
+  {
+    nombre: "Grupo F",
+    equipos: ["Países Bajos", "Japón", "Túnez", "Por definir (UEFA B)"]
+  },
+  {
+    nombre: "Grupo G",
+    equipos: ["Bélgica", "Irán", "Egipto", "Nueva Zelanda"]
+  },
+  {
+    nombre: "Grupo H",
+    equipos: ["España", "Uruguay", "Arabia Saudita", "Cabo Verde"]
+  },
+  {
+    nombre: "Grupo I",
+    equipos: ["Francia", "Senegal", "Noruega", "Por definir (Intercontinental 2)"]
+  },
+  {
+    nombre: "Grupo J",
+    equipos: ["Argentina", "Austria", "Argelia", "Jordania"]
+  },
+  {
+    nombre: "Grupo K",
+    equipos: ["Portugal", "Colombia", "Uzbekistán", "Por definir (Intercontinental 1)"]
+  },
+  {
+    nombre: "Grupo L",
+    equipos: ["Inglaterra", "Croacia", "Panamá", "Ghana"]
+  }
+]
